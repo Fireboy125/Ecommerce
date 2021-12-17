6ff8f5916df852b9b395c5335c23b55f597cd64c
Abdumuneeb <muneebafridi07@gmail.com>
2021-12-17 22:49:45 +0500

    Whole project

--- src/components/LoginForm/Form.js
+++ src/components/LoginForm/Form.js
@@ -15,43 +15,49 @@ const Form = ({ Login, error }) => {
 
   return (
     <div className={style.form_wrapper}>
-    <form onSubmit={submitHandler}>
-      <div className={style.form_inner}>
-        <h2>Login</h2>
-        {error !== "" ? <div className="error">{error}</div> : ""}
-        <div className={style.form_group}>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={details.name}
-            onChange={handleChange}
-          />
+      <form onSubmit={submitHandler}>
+        <div className="d-flex flex-column justifiy-content-center">
+          <h3> Credentails for Checking Purpose</h3>
+          <h5>Abdul Muneeb Khan </h5>
+          <h5> admin@gmail.com</h5>
+          <h5>admin123 </h5>
         </div>
-        <div className={style.form_group}>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={details.email}
-            onChange={handleChange}
-          />
+        <div className={style.form_inner}>
+          <h2>Login</h2>
+          {error !== "" ? <div className="error">{error}</div> : ""}
+          <div className={style.form_group}>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={details.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={style.form_group}>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={details.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={style.form_group}>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={details.password}
+              onChange={handleChange}
+            />
+          </div>
+          <input type="submit" value="Submit" />
         </div>
-        <div className={style.form_group}>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={details.password}
-            onChange={handleChange}
-          />
-        </div>
-        <input type="submit" value="Submit" />
-      </div>
-    </form>
+      </form>
     </div>
   );
 };
